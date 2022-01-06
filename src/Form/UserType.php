fbfa216619c9499c0f898580dc4fd125e87015f6
Benoit Baudrin-VG <benoit.bvg@gmail.com>
2022-01-06 22:43:51 +0100

    front registration + logo accueil

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,14 +10,16 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
             ->add('name')
             ->add('firstname')
             ->add('adress')
