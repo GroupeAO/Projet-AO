7ae0132bd81f6495c455d6885459bfc544c5ff1c
Benoit Baudrin-VG <benoit.bvg@gmail.com>
2022-01-06 15:26:17 +0100

    menu top quasi-fini, intégration pages informations 2/4

--- src/Controller/InformationsController.php
+++ src/Controller/InformationsController.php
@@ -9,10 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class InformationsController extends AbstractController
 {
     #[Route('/about_us', name: 'about_us')]
-    public function index(): Response
+    public function aboutUs(): Response
     {
         return $this->render('informations/aboutus.html.twig', [
-            'controller_name' => 'InformationsController',
+        ]);
+    }
+
+    #[Route('/informations', name: 'informations')]
+    public function informations(): Response
+    {
+        return $this->render('informations/informations.html.twig', [
+        ]);
+    }
+
+    #[Route('/credits', name: 'credits')]
+    public function credits(): Response
+    {
+        return $this->render('informations/credits.html.twig', [
+        ]);
+    }
+    #[Route('/contact_us', name: 'contact_us')]
+    public function contactUs(): Response
+    {
+        return $this->render('informations/contactus.html.twig', [
+        ]);
+    }
+    #[Route('/donate', name: 'donate')]
+    public function donate(): Response
+    {
+        return $this->render('informations/donate.html.twig', [
         ]);
     }
 }
