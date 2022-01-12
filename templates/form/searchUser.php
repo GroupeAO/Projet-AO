4bf9e85c3dcf996701d7626c68e1cb2b6025f672
Benoit BAUDRIN-VG <benoit.bvg@gmail.com>
2022-01-12 17:19:18 +0100

    diverse front

--- templates/form/searchUser.php
+++ templates/form/searchUser.php
@@ -1,10 +1,17 @@
         
         <form action="{{path('search')}}" method="post" class="loginForm">
-            <div class="row justify-content-center mb-5">
-                <div class="col-auto">  
-                <input type="search" name="search" id="search">
-                <button type="submit" class="btn btn-primary">rechercher un utilisateur</button>
+
+        <div class="text-white bg-secondary mb-3 text-center">
+            <div class="card-header">Rechercher un(e) utilisateur(rice)</div>
+            <div class="card-body row justify-content-center">
+                <div class="col-md-6">
+                    <label for="search">Nom et/ou Prénom :</label>
+                    <input type="search" name="search" id="search">
+                    <button type="submit" class="btn btn-light">Rechercher</button>
+                </div>
+                
             </div> 
+        </div>
         </form>
     
 
---
+++ templates/home/homeCards.php
@@ -0,0 +1,24 @@
+<div class="row row-flex justify-content-center homeCard">
+    <div class="col-md-5">
+        <div class="text-white bg-primary mb-3">
+            <div class="card-header">Aides Opératoires</div>
+            <div class="card-body">
+                <p class="card-text">Indiquez vos coordonnées, vos disponibilités et un chirurgien entrera directement en contact avec vous.</p>
+            </div>
+            <div class="formSubmit">
+                <button type="button" class="btn btn-light bt-lg mb-3">Inscrivez-vous</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-5">
+        <div class="text-white bg-secondary mb-3"  style="height:100%">
+            <div class="card-header">Chirurgiens</div>
+            <div class="card-body">
+                <p class="card-text">Créez votre compte et recherchez un(e) IBODE ou IDE en quelques clics.</p>
+            </div>
+            <div class="formSubmit">
+                <button type="button" class="btn btn-light bt-lg mb-3">Inscrivez-vous</button>
+            </div>
+        </div>
+    </div>
+</div>
