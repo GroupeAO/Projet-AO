a804d9fa356395390c68d37bc328bd5dc8d54195
Benoit BAUDRIN-VG <benoit.bvg@gmail.com>
2022-01-13 16:39:41 +0100

    showHidePassword

--- templates/home/homeCards.php
+++ templates/home/homeCards.php
@@ -1,5 +1,5 @@
 <div class="row row-flex justify-content-center">
-    <div class="col-md-5">
+    <div class="col-md-4">
         <div class="text-white bg-primary mb-3 h-100">
             <div class="card-header">Aides Opératoires</div>
             <div class="card-body">
@@ -10,13 +10,13 @@
             </div>
         </div>
     </div>
-    <div class="col-md-5">
+    <div class="col-md-4 flex">
         <div class="text-white bg-secondary mb-3 h-100">
             <div class="card-header">Chirurgiens</div>
             <div class="card-body">
                 <p class="card-text">Créez votre compte et recherchez un(e) IBODE ou IDE en quelques clics.</p>
             </div>
-            <div class="formSubmit">
+            <div class="formSubmit align-self-end">
                 <button type="button" class="btn btn-light bt-lg mb-3">Inscrivez-vous</button>
             </div>
         </div>
