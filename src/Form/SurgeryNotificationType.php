c4a70e34454cdd0984e7c6d965a74ed6d4aa439b
GroupeAO <projetwf3.ao@gmail.com>
2022-01-13 16:41:07 +0100

    check availibity date +front

--- src/Form/SurgeryNotificationType.php
+++ src/Form/SurgeryNotificationType.php
@@ -6,6 +6,8 @@ use App\Entity\SurgeryNotification;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,18 +19,28 @@ class SurgeryNotificationType extends AbstractType
             ->add('startDate', DateTimeType::class, [
                 'widget' => 'single_text',
                 'input'  => 'datetime_immutable',
-                
+                'label'  => 'Date de début'
             ])
             ->add('enDate', DateTimeType::class, [
                 'widget' => 'single_text',
                 'input'  => 'datetime_immutable',
-                
+                'label'  => 'Date de fin'
             ])
-            ->add('speciality')
-            ->add('description')
-            ->add('numberAoNeeded')
-            ->add('clinicName')
-            ->add('clinicZipCode')
+            ->add('speciality', TextType::class,[
+                'label'  => 'Spécialité'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Decrivez-la procédure'
+            ])
+            ->add('numberAoNeeded', TextType::class,[
+                'label'  => 'Nombre d\'aide opératoire nécéssaire'
+            ])
+            ->add('clinicName', TextType::class,[
+                'label'  => 'Nom de la clinique'
+            ])
+            ->add('clinicZipCode', TextType::class,[
+                'label'  => 'Code postal de la clinique'
+            ])
         ;
     }
 
