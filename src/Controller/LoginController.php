d8b920c037789b418694daf2663ba04dd49d4677
GroupeAO <97027364+GroupeAO@users.noreply.github.com>
2022-01-14 20:18:28 +0100

    login after registartion working

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,29 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+  private $requestStack;
+
+  public function __construct(RequestStack $requestStack)
+  {
+      $this->requestStack = $requestStack;
+  }
     #[Route('/login', name: 'login')]
     public function index(AuthenticationUtils $authenticationUtils, ): Response
-    {
-          //getting the errors during connexion
-        $error= $authenticationUtils->getLastAuthenticationError();
-        
-          //  we get the last_username entered by the user 
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
-        ]);
-    }
-
-    #[Route('/login_registartion', name: 'login_registration')]
-    public function loginRegistration(AuthenticationUtils $authenticationUtils, ): Response
     {
           //getting the errors during connexion
         $error= $authenticationUtils->getLastAuthenticationError();
