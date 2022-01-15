d00dfe93f1cc2c670326fab12df579bf77eb3f9d
Benoit BAUDRIN-VG <benoit.bvg@gmail.com>
2022-01-15 10:15:17 +0100

    encore du template

--- templates/home/homeCards.php
+++ templates/home/homeCards.php
@@ -1,6 +1,6 @@
 <div class="row row-flex justify-content-center">
-    <div class="col-md-4">
-        <div class="text-white bg-primary mb-3 h-100">
+    <div class="col-md-4 flex mb-3">
+        <div class="text-white bg-primary h-100">
             <div class="card-header">Aides Opératoires</div>
             <div class="card-body">
                 <p class="card-text">Indiquez vos coordonnées, vos disponibilités et un chirurgien entrera directement en contact avec vous.</p>
@@ -10,8 +10,8 @@
             </div>
         </div>
     </div>
-    <div class="col-md-4 flex">
-        <div class="text-white bg-secondary mb-3 h-100">
+    <div class="col-md-4 flex mb-3">
+        <div class="text-white bg-secondary h-100">
             <div class="card-header">Chirurgiens</div>
             <div class="card-body">
                 <p class="card-text">Créez votre compte et recherchez un(e) IBODE ou IDE en quelques clics.</p>
